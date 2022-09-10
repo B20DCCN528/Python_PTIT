7e3910da09250707b0076a15210d145da6d1160f
quang1105 <89206951+quang1105@users.noreply.github.com>
2022-09-10 09:10:11 +0700

    10/09/2022

---
+++ Emirpnumber.py
@@ -0,0 +1,16 @@
+a = [0] * 1000001
+def era() :
+    a[0] = a[1] = 1
+    for i in range(2, 1000) :
+        if a[i] == 0 :
+            for j in range(i * i, 1000001, i) : a[j] = 1
+era()
+t = int(input())
+
+for i in range(t) :
+    n = int(input())
+    for i in range(1, n) :
+        k = int(str(i)[::-1])
+        if k > i and k < n and a[i] == 0 and a[k] == 0 :
+            print(i, k, end = ' ')
+    print()
---
+++ Timsolonnhat.py
@@ -0,0 +1,15 @@
+from curses.ascii import isalpha
+
+
+t = int(input())
+for i in range(t) :
+    n = input()
+    n = n + 'z'
+    ans = 0
+    s = 0
+    for i in range(len(n)) :
+        if n[i].isalpha() :
+            if i != 0 and n[i - 1].isdigit() : ans = max(ans, s)
+            s = 0
+        else : s = s * 10 + int(n[i])
+    print(ans) 
