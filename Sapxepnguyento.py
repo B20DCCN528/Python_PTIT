404d5d3685d66efed49c14c9d95db6f3090f5174
quang1105 <89206951+quang1105@users.noreply.github.com>
2022-10-03 22:34:10 +0700

    03/10/2022

---
+++ Sapxepnguyento.py
@@ -0,0 +1,23 @@
+import math
+def nguyento(n) :
+    if n < 2 : return False
+    for i in range(2, int(math.sqrt(n)) + 1) :
+        if n % i == 0 : return False
+    return True
+n = int(input())
+a = [int(x) for x in input().split()]
+b, c, d = [], [], [0] * n
+for i in range(n) :
+    if nguyento(a[i]) == 0 : b.append(a[i])
+    else : 
+        c.append(a[i])
+        d[i] = 1
+c = sorted(c)
+p1, p2 = 0, 0
+for i in range(n) :
+    if d[i] == 1 : 
+        print(c[p2], end = ' ')
+        p2 += 1
+    else :
+        print(b[p1], end = ' ')
+        p1 += 1
